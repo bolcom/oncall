b9bd82599f2d891d2e82ba3823491a456b011354
William Leese <wleese@bol.com>
2018-01-10 10:43:05 +0100

    use moment-tz-data-full.js

--- src/oncall/ui/__init__.py
+++ src/oncall/ui/__init__.py
@@ -23,7 +23,7 @@ assets_env = AssetsEnvironment(path.join(STATIC_ROOT, 'static'),
 
 assets_env.register('libs', Bundle(
     'js/jquery-2.1.4.min.js', 'js/handlebars.min.js', 'js/bootstrap.min.js',
-    'js/moment.js', 'js/moment-timezone.js', 'js/moment-tz-data.js',
+    'js/moment.js', 'js/moment-timezone.js', 'js/moment-tz-data-full.js',
     'js/typeahead.js',
     output='bundles/libs.js'))
 assets_env.register('oncall_js', Bundle(
@@ -63,6 +63,7 @@ INDEX_CONTENT_SETTING = {
 SLACK_INSTANCE = None
 HEADER_COLOR = None
 IRIS_PLAN_SETTINGS = None
+USERCONTACT_UI_READONLY = None
 
 
 def index(req, resp):
@@ -75,6 +76,7 @@ def index(req, resp):
         missing_number_note=INDEX_CONTENT_SETTING['missing_number_note'],
         header_color=HEADER_COLOR,
         iris_plan_settings=IRIS_PLAN_SETTINGS,
+        usercontact_ui_readonly=USERCONTACT_UI_READONLY,
         footer=INDEX_CONTENT_SETTING['footer'],
         timezones=SUPPORTED_TIMEZONES
     )
@@ -118,9 +120,11 @@ def init(application, config):
     global SLACK_INSTANCE
     global HEADER_COLOR
     global IRIS_PLAN_SETTINGS
+    global USERCONTACT_UI_READONLY
     SLACK_INSTANCE = config.get('slack_instance')
     HEADER_COLOR = config.get('header_color', '#3a3a3a')
     IRIS_PLAN_SETTINGS = config.get('iris_plan_integration')
+    USERCONTACT_UI_READONLY = config.get('usercontact_ui_readonly', True)
 
     application.add_sink(index, '/')
     application.add_route('/static/bundles/{filename}',
